7a7bdcd6d32a2426925c1db824fab71ca82b9ce6
Rebecka Reitmaier <trreitmaier@msn.com>
2020-09-26 13:05:25 +0200

    simple cube

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,28 @@
-import lodash, { runInContext } from 'lodash';
-import hello from './testThree'
 import * as THREE from 'three';
-import { CircleBufferGeometry } from 'three';
-
 
 function run() {
-  function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
-  }
 
-    const element = document.createElement('div');
+    function animate() {
+      requestAnimationFrame( animate );
+      renderer.render( scene, camera );
+    }
   
-    element.innerHTML = lodash.join(['Hello', 'webpack'], ' ');
+      var scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+      camera.position.z = 5;
+
+      const geometry: THREE.BoxGeometry = new THREE.BoxGeometry(1,1,1)
+      const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#eab8b2', wireframe: true })
+      const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
+      scene.add(cube)
+      cube.position.set(0, 0, 0);
   
-    console.log('hi there 34');
-
-    hello('rebecka 2ß')
-
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-    var geometry = new THREE.BoxGeometry();
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );  
-    camera.position.z = 5;
-
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-
-    animate();
-
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( renderer.domElement );
+  
+      animate();
   }
   
 run()
